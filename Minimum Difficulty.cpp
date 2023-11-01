ddfa5e6d97f638c300b97df29a40d821b96debb4
bhavjyot2912 <Bhavjyot.Singh@iiitb.org>
2023-11-01 19:07:21 +0530

    "Ladder"

--- Minimum Difficulty.cpp
+++ Minimum Difficulty.cpp
@@ -3,5 +3,20 @@ using namespace std;
 #define ll long long
 
 int main(){
-    
+    ll n;
+    cin>>n;
+    vector<ll> vec(n);
+    for(int i=0;i<n;i++){
+        cin>>vec[i];
+    }
+    ll maxi=0;
+    for(int i=1;i<n;i++){
+        maxi=max(maxi, vec[i]-vec[i-1]);
+    }
+    ll mini=1000;
+    for(int i=1;i<n-1;i++){
+        mini=min(mini, vec[i+1]-vec[i-1]);
+    }
+    cout<<max(mini, maxi)<<endl;
+    return 0;
 }
